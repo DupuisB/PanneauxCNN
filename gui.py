Backprog pour Greyed, Softmax Slight gui improvements / fix Cross-entropy fonctionnel

gui.py
@@ -1,32 +1,35 @@
 import random
 
+import threading
 import network
 from loaders.mnist_loader import *
 from loaders.pano_loader import *
 from utils.activation_functions import *
 from layers import *
 from utils.cost_functions import *
+from utils.classes import *
 
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog as fd
 from PIL import ImageTk, Image
 
+convert = {'Quadratique': QuadraticCost, 'Quasratique2': QuadraticCost2, 'Cross-entropy': CrossEntropyCost}
+convert_inv = {QuadraticCost: 'Quadratique', QuadraticCost2: 'Quadratique2', CrossEntropyCost: 'Cross-entropy'}
+
 
 class Software(object):
 
-    def __init__(self, network, background='#FFEFDB', size='1180x680', windows_title='CNN - Dupuis Benjamin'):
+    def __init__(self, network, background='#FFEFDB', windows_title='CNN - Dupuis Benjamin'):
         self.net = network
         self.data = None
-
         self.title = windows_title
         self.back = background
-        self.size = size
         self.font = ('arial', 11, 'normal')
 
         self.root = tk.Tk()
 
-        self.root.geometry(self.size)
+        self.root.state('zoomed')
         self.root.configure(background=self.back)
         self.root.title(self.title)
         self.root['padx'] = 10
@@ -46,8 +49,8 @@ class Software(object):
 
         tk.Label(self.networkFrame, text="Fonction Cout", font=self.font, bg=self.back).grid(row=2, column=1,
                                                                                              padx=10, pady=10)
-        self.cost_func = ttk.Combobox(self.networkFrame, values=['Quadratique', 'Cross-entropy'])
-        self.cost_func.insert(0, 'Quadratique')
+        self.cost_func = ttk.Combobox(self.networkFrame, values=list(convert.keys()))
+        self.cost_func.insert(0, convert_inv[self.net.cost])
         self.cost_func.grid(row=2, column=2, padx=10, pady=10)
 
         tk.Label(self.networkFrame, text="Data Loader", font=self.font, bg=self.back).grid(row=3, column=1, padx=10,
@@ -71,10 +74,10 @@ class Software(object):
         # Instructions
         self.text = tk.LabelFrame(self.root, text="Instructions", relief=tk.RIDGE, bg=self.back)
         self.text.grid(row=2, column=1)
-        tk.Label(self.text, text="Couches:\n\n"
-                                 "Convolution, Dense, Reshape, Softmax\n\n"
-                                 "", font=self.font, bg=self.back).grid(row=4, column=1,
-                                                                        pady=10, padx=10)
+        tk.Label(self.text, text="Couches:\nConvolution, Dense, Reshape, Maxpooling, Softmax\n\n"
+                                 "A modifier:\n2 images, graph, truc a droite pcq vide\n\n"
+                                 "Reseaux dispo:\ngrey80, ...", font=self.font, bg=self.back).grid(row=4, column=1,
+                                                                                                   pady=10, padx=10)
 
         # Settings frame (parametres d'entrainement)
         self.settingsFrame = tk.LabelFrame(self.root, text="Paramètres entrainement", relief=tk.RIDGE, bg=self.back)
@@ -129,7 +132,6 @@ class Software(object):
                                           command=self.eval_test)
         self.eval_test_button.grid(row=5, column=1, padx=10, pady=10)
 
-
         # Image frame
         self.imageFrame = tk.LabelFrame(self.root, text="Image", relief=tk.RIDGE, bg=self.back)
         self.imageFrame.grid(row=1, column=3, padx=5)
@@ -150,6 +152,8 @@ class Software(object):
         self.loadImage = tk.Button(self.imageFrame, text='Load Image', bg=self.back, font=self.font,
                                    command=self.getImage)
         self.loadImage.grid(row=3, column=1, padx=10, pady=10)
+
+        self.imageLabel = None  # Initialisation pour eviter crash
 
         self.randomImage = tk.Button(self.imageFrame, text='Load random', bg=self.back, font=self.font,
                                      command=self.loadRandom)
@@ -187,7 +191,6 @@ class Software(object):
         return eval(self.loaderTK.get())
 
     def getCost(self):
-        convert = {'Quadratique': QuadraticCost, 'Cross-entropy': CrossEntropyCost}
         userInput = self.cost_func.get()
         return convert[userInput]
 
@@ -206,11 +209,11 @@ class Software(object):
         return float(self.eta.get())
 
     def getMonitoring(self):
-        return self.train_acc, self.eval_acc
+        return self.train_acc.get(), self.eval_acc.get()
 
     def updateImage(self, image):
         self.image = image
-        displayReady = np.squeeze(255*image).astype(np.uint8)
+        displayReady = np.squeeze(255 * image).astype(np.uint8)
         self.imageDisplayed = Image.fromarray(displayReady)
         self.imgDisplayed = ImageTk.PhotoImage(image=self.imageDisplayed.resize((224, 224)))
         self.imageTK.configure(image=self.imgDisplayed)
@@ -221,21 +224,25 @@ class Software(object):
         )
         path = fd.askopenfilename(filetypes=filetypes)
         self.updateImage(Image.open(path))
+        self.imageLabel = None
 
     def loadRandom(self):
         if self.data is None:
             self.data = self.getLoader()()[0]
-        img = self.data[np.random.choice(self.data.shape[0])][:,:,:,0]
-        self.updateImage(img)
+        img = self.data[np.random.choice(self.data.shape[0])]
+        self.imageLabel = img[0, 0, 0, 0]
+        self.updateImage(img[:, :, :, 0])
 
     def feedforward(self):
-        print('ff...')
         ff = self.net.feedforwardClasse(np.asarray(self.image))
-        self.log(f'Probable: {ff}')
-        self.output['text'] = f'Prévision: \n{ff}'
+        self.log(f'P:{ff}')
+        if self.imageLabel is not None:
+            self.output['text'] = f'Prévision:\n{ff}\n\nVrai:\n{classes_fr()[self.imageLabel]}'
+        else:
+            self.output['text'] = f'Prévision:\n{ff}'
 
     def create(self):
-        self.net = network.Network(layers=self.getNetwork(), cost=self.getCost())
+        self.net = network.Network(layers=self.getNetwork(), cost=self.getCost(), loader=self.getLoader())
         self.log('Network created')
 
     def save(self):
@@ -248,12 +255,13 @@ class Software(object):
         self.networkTK.delete("0.0", tk.END)
 
         for layer in self.net.layers:
-            self.networkTK.insert(tk.END, '\n' + layer.name())
+            self.networkTK.insert(tk.END, layer.name() + '\n')
 
     def train(self):
-        train_retour = self.net.train(loader=self.getLoader(), epochs=self.getEpochs(),
+        m = self.getMonitoring()
+        train_retour = self.net.train(epochs=self.getEpochs(),
                                       mini_batch_size=self.getMiniSize(), eta=self.getEta(),
-                                      train_accuracy=self.getMonitoring()[0], test_accuracy=self.getMonitoring()[1])
+                                      train_accuracy=m[0], test_accuracy=m[1])
         return train_retour
 
     def eval_train(self):
@@ -268,20 +276,8 @@ class Software(object):
         self.root.destroy()
 
 
-# test = Software(network=network.Network(
-#    layers=[Maxpooling((64, 64, 3), 16), Convolution((4, 4, 3), 3, 5, sigmoid), Reshape((2, 2, 5), (2*2*5, 1)), Dense(20, 164, sigmoid)]),
-#    loader=pano_loader)
-
-# test = Software(network=network.Network(layers=[Maxpooling((64, 64, 3), 2), Convolution((32, 32, 3), 4, 5, sigmoid), Reshape((29, 29, 5), (5 * 29 * 29, 1)),
-#            Dense(29*29* 5, 512, sigmoid), Dense(512, 164, sigmoid)]), loader=pano_loader_small)
-
-"""
-test = Software(
-   network=network.Network(layers=[Convolution((64, 64, 3), 4, 5, sigmoid), Reshape((61, 61, 5), (5 * 61 * 61, 1)),
-                                  Dense(61 * 61 * 5, 164, sigmoid)]),
- loader=pano_loader_medium_32)
-"""
 if __name__ == '__main__':
     test = Software(network=network.Network(loader=pano_loader_grey,
-                                layers=[Convolution((32, 32, 1), 4, 5, sigmoid), Reshape((29, 29, 1), (29 * 29 * 1, 1)),
-                                        Dense(29 * 29 * 1, 43, sigmoid)]))
+                                            layers=[Convolution((32, 32, 1), 4, 5, sigmoid),
+                                                    Reshape((29, 29, 5), (29 * 29 * 5, 1)),
+                                                    Dense(29 * 29 * 5, 43, sigmoid), Softmax()], cost=CrossEntropyCost))

layers.py
@@ -128,7 +128,6 @@ class Reshape(Layer):
 
 class Softmax(Layer):
     """Normalise pour avoir somme des sorties = 1"""
-
     def __init__(self):
         pass
 
@@ -137,14 +136,17 @@ class Softmax(Layer):
         self.sortie = expo / np.sum(expo)
         return self.sortie
 
-    def backpropagation(self, grad_sortie, eta):
-        return grad_sortie
+    def backpropagation(self, eta, nabla_sortie):
+        jacobian = np.diag(self.sortie.flatten()) - np.outer(self.sortie, self.sortie)
+        nabla_entree = jacobian @ nabla_sortie
+        return nabla_entree
 
     def name(self):
         return "Softmax"
 
 
-class Maxpooling():
+
+class Maxpooling(Layer):
     """filtre carre, cote*cote, aucun parametre (donc rien a update en backprop), filtre_cote divise la hauteur ET la largeur"""
 
     def __init__(self, entree_dim, filtre_cote):
@@ -184,16 +186,22 @@ class Maxpooling():
 
 
 class Greyed(Layer):
-    """Seulement en premiere couche ! (pas le bon gradient)"""
-
-    def __init__(self):
-        pass
+    """Convertit l'image en niveaux de gris"""
+    def __init__(self, first = True):
+        self.rgb_weights = np.array([0.2989, 0.5870, 0.1140])
+        self.first = first
 
     def feedforward(self, entree):
-        return (entree[..., :3] @ [0.2989, 0.5870, 0.1140])[..., None]  # [..., None] pour le format la dim manquante
+        self.entree = entree
+        return (entree[..., :3] @ self.rgb_weights)[..., None]  # [..., None] pour ajouter la dimension manquante
 
-    def backpropagation(self, grad_sortie, eta):
-        return grad_sortie
+    def backpropagation(self, nabla_sortie, eta):
+        if self.first:
+            return nabla_sortie
+        nabla_entree = np.zeros_like(self.entree)
+        nabla_entree[..., :3] = (nabla_sortie[..., 0, None] @ self.rgb_weights[None, :])
+        return nabla_entree
 
     def name(self):
         return "Greyed"
+

network.py
@@ -39,7 +39,6 @@ class Network(object):
     def feedforwardClasse(self, entree):
         """
         Renvoie la classe pour un vecteur d'entree a
-        Ne marche pas
         """
         sortie = self.feedforward(entree)
         return classes_fr()[np.argmax(sortie)]
@@ -69,7 +68,7 @@ class Network(object):
 
             monitor_batch = 0  #
             for batch in batches:
-                if monitor_batch % 100 == 0:  #
+                if monitor_batch % 1000 == 0:  #
                     print(f'Batch: {monitor_batch}')  #
                 self.train_batch(batch, eta)
                 monitor_batch += 1  #
@@ -83,8 +82,8 @@ class Network(object):
 
     def train_batch(self, batch, eta):
         batch_size = len(batch[0])
-        for truc in batch:
-            image, label = truc[:, :, :, 0], int_to_vect(int(truc[0, 0, 0, 0]), self.nb_classes)
+        for entree in batch:
+            image, label = entree[:, :, :, 0], int_to_vect(int(entree[0, 0, 0, 0]), self.nb_classes)
             output = self.feedforward(image)
             nabla = self.cost.prime(output, label)
             for layer in reversed(self.layers):
@@ -101,13 +100,12 @@ class Network(object):
         print('Evaluating accuracy...')
         results = [int(np.argmax(self.feedforward(couple[:, :, :, 0])) == int(couple[0, 0, 0, 0])) for couple in data]
         juste, total = sum(results), len(results)
-        # print(results)
         print(f"Accuracy: {juste}/{total} ou {juste / total:.4%} {add_str}")
         return juste, total
 
     def save(self, nom):
         """"
-        Sauvegardes sous ..\network\filename
+        Sauvegardes sous ../network/filename
         """
         with open('./networks/' + nom, "wb") as f:
             pickle.dump(self, f)
@@ -129,7 +127,7 @@ def load(nom):
 ####Fonctions randoms
 def int_to_vect(j, size):
     """
-    vecteur size*1, 1.0 si l = j, 0 sinon
+    delta i,j dans \mathbb{R}^n
     """
     e = np.zeros((size, 1))
     e[j] = 1.0

utils/cost_functions.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-
-#### Define the quadratic and cross-entropy cost functions
 class Cost():
     pass
 
@@ -20,7 +18,7 @@ class QuadraticCost(Cost):
         """
         Return the cost associated with an output ``a`` and desired output ``y``.
         """
-        return np.linalg.norm(a - y) #(a - y, 'norm name')
+        return np.linalg.norm(a - y)
 
     @staticmethod
     def prime(a, y):
@@ -29,18 +27,14 @@ class QuadraticCost(Cost):
 class CrossEntropyCost(Cost):
     @staticmethod
     def fn(a, y):
-        """Return the cost associated with an output ``a`` and desired output
-        ``y``.  Note that np.nan_to_num is used to ensure numerical
-        stability.  In particular, if both ``a`` and ``y`` have a 1.0
-        in the same slot, then the expression (1-y)*np.log(1-a)
-        returns nan.  The np.nan_to_num ensures that that is converted
-        to the correct value (0.0).
-
+        """
+        Return the cost associated with an output ``a`` and desired output ``y``.
         """
         return np.sum(np.nan_to_num(-y * np.log(a) - (1 - y) * np.log(1 - a)))
 
     @staticmethod
     def prime(a, y):
-        a = np.clip(a, 1e-7, 1 - 1e-7)
-        y = np.clip(y, 1e-7, 1 - 1e-7)
-        return ((1 - y) / (1 - a) - y / a) / np.size(y)
+        """
+        Return the gradient of the cost function with respect to the output ``a`` and desired output ``y``.
+        """
+        return (a - y) / (a * (1 - a))